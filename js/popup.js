eca750dd04d0ac5932bb3cb914fd08e4098ed7ed
beattim26 <beattimy@yandex.ru>
2018-07-06 16:05:23 +0300

    Завершил js

--- js/popup.js
+++ js/popup.js
@@ -5,17 +5,20 @@ var login = popup.querySelector("[name=login]");
 var form = popup.querySelector("form");
 var email = popup.querySelector("[name=email]");
 var storage = localStorage.getItem("login");
+var overlay = document.querySelector(".overlay");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
+  overlay.classList.add("overlay-show");
   login.focus();
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
-   popup.classList.remove("modal-error");
+  popup.classList.remove("modal-error");
+  overlay.classList.remove("overlay-show");
 });  
 
 window.addEventListener("keydown", function (evt) {
@@ -25,14 +28,24 @@ window.addEventListener("keydown", function (evt) {
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
       popup.classList.remove("modal-error");
+      overlay.classList.remove("overlay-show");
     }
   }
 });
 
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+  overlay.classList.remove("overlay-show");
+});
+
 form.addEventListener("submit", function (evt) {
   if(!login.value || !email.value) {
-  evt.preventDefault();
-  popup.classList.add("modal-error");
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
   } else {
     localStorage.getItem("login", login.value);
   }
